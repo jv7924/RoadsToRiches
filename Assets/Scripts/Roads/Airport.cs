5246d8ba60e5bda0346cc2ce110df0401cd5ba08
Trevor Vahan <tvahan@uci.edu>
2023-11-07 17:15:27 -0800

    Casino and Airport

---
+++ Assets/Scripts/Roads/Airport.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Airport : Road
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        up    = new KeyValuePair<bool, GameObject>(true, null);
+        down  = new KeyValuePair<bool, GameObject>(true, null);
+        left  = new KeyValuePair<bool, GameObject>(true, null);
+        right = new KeyValuePair<bool, GameObject>(true, null);
+        rotation = 0;
+    }
+}
