d6665d1200ef81d09fef8f83391b2c562f21742d
brayww <birdenthebirdman@gmail.com>
2023-11-27 00:25:58 -0800

    ANIMATED DYNAMIC CARDBAR

--- Assets/Scripts/UI/MouseOverShrink.cs
+++ Assets/Scripts/UI/MouseOverShrink.cs
@@ -7,25 +7,60 @@ using UnityEngine.EventSystems;
 public class MouseOverShrink : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     [SerializeField]
-    private float shrink;
+    private float shrinkSize, shrinkRate, shrinkInterval;
 
     [SerializeField]
     private CanvasScaler ui;
 
+    private bool hovered;
+
     void Start()
     {
-        shrink = ui.scaleFactor;
+        hovered = false;
+        shrinkSize = ui.scaleFactor;
+    }
+
+    private IEnumerator Enlarge()
+    {
+        while(hovered)
+        {
+            if(ui.scaleFactor < 1.0f)
+            {
+                ui.scaleFactor += shrinkRate;
+            }
+            else
+            {
+                ui.scaleFactor = 1.0f;
+            }
+            yield return new WaitForSeconds(shrinkInterval);
+        }
+    }
+
+    private IEnumerator Shrink()
+    {
+        while(!hovered)
+        {
+            if(ui.scaleFactor > shrinkSize)
+            {
+                ui.scaleFactor -= shrinkRate;
+            }
+            else
+            {
+                ui.scaleFactor = shrinkSize;
+            }
+            yield return new WaitForSeconds(shrinkInterval);
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
     {
-        ui.scaleFactor = 1.0f;
-        Debug.Log(ui.scaleFactor);
+        hovered = true;
+        StartCoroutine(Enlarge());
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
-        ui.scaleFactor = shrink;
-        Debug.Log(ui.scaleFactor);
+        hovered = false;
+        StartCoroutine(Shrink());
     }
 }
